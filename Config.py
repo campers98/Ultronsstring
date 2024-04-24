a817c70e91b7e0443909e4b0cb01adb76d27e360
campers98 <140605897+campers98@users.noreply.github.com>
2024-04-24 11:08:46 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,15 +4,15 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 0))
+        API_ID = int(os.environ.get('API_ID', 24853835))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = os.environ.get('API_HASH', None)
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+    API_HASH = os.environ.get('API_HASH', "bdd57a7f24d7d1f59b82f937289e4abb")
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', "6998449442:AAGIrcg6vJyp0e-bJES3VtErA38Wlyr2hlM")
+    DATABASE_URL = os.environ.get('DATABASE_URL', "mongodb+srv://Singlecell94:Single_1122334455@cluster234.n2vmnn3.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URL = DATABASE_URL.replace("MONGO_DB_URL", "mongodb")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    MUST_JOIN = os.environ.get('MUST_JOIN', None)
+    MUST_JOIN = os.environ.get('MUST_JOIN', "https://t.me/SVD_Squad_Gamerz")
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
